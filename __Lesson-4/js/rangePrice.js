62cd50508743e25d4165826800a5b4b3e8e16c2e
Venchic <48753123+Venchic@users.noreply.github.com>
2021-02-14 22:14:36 +0300

    123

--- __Lesson-4/js/rangePrice.js
+++ __Lesson-4/js/rangePrice.js
@@ -2,12 +2,11 @@ $( function() {
     $( "#slider-range" ).slider({
       range: true,
       min: 0,
-      max: 500,
-      values: [ 75, 300 ],
+      max: 1000,
+      values: [ 52, 400 ],
       slide: function( event, ui ) {
         $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) + " - $" + $( "#slider-range" ).slider( "values", 1 ) );
   } );
